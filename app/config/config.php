0140167efc93f39f585dffd03921ef10f216a7e7
muchemikelvin <kelvinmuchemi111@gmail.com>
2025-01-29 18:47:13 +0300

    footer manipulation

--- app/config/config.php
+++ app/config/config.php
@@ -3,19 +3,16 @@
 date_default_timezone_set("Asia/Kolkata");
 
 // define the site root
-define('SITE_ROOT', 'http://localhost/ample/');
+define('SITE_ROOT', 'http://localhost/bahati%20inventory/');
 
 
 
 // Database Information
 // Database Hostname
-define('DATABASE_HOST','localhost');
+define('DATABASE_HOST', 'localhost');
 // Database Username
-define('DATABASE_USER','root');
+define('DATABASE_USER', 'root');
 // Database Name
 define('DATABASE_NAME', 'ample');
 // Database DB_PASS
-define('DATABASE_PASS','');
-
-
- ?>
+define('DATABASE_PASS', '');
